5e7635a504167f978866b2e1e4414213c72130fa
taiyq <taiyq@inspur.com>
2020-01-05 15:51:11 +0800

    既往史、疾病

--- src/view/symptom/past-history.vue
+++ src/view/symptom/past-history.vue
@@ -1,6 +1,67 @@
-
+<style lang="less">
+    @import "./past-history.less";
+</style>
 <template>
   <div class="content">
-    123
+      <div class="query-con">
+        <label>症状：</label>
+        <Input value="" placeholder="请输入症状" style="width: 300px" />
+        <Button type="primary" id="queryBtn">查询</Button>
+      </div>
+      <div class="result-con">
+           <Table border :columns="columns1" :data="data1"></Table>
+      </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      columns1: [
+        {
+          title: '姓名',
+          key: 'name'
+        },
+        {
+          title: '性别',
+          key: 'sex'
+        },
+        {
+          title: '出生日期',
+          key: 'birthday'
+        },
+        {
+          title: '既往史',
+          key: 'past_history'
+        }
+      ],
+      data1: [
+        {
+          name: 'John Brown',
+          sex: 18,
+          past_history: 'New York No. 1 Lake Park',
+          birthday: '2016-10-03'
+        },
+        {
+          name: 'John Brown',
+          sex: 18,
+          past_history: 'New York No. 1 Lake Park',
+          birthday: '2016-10-03'
+        },
+        {
+          name: 'John Brown',
+          sex: 18,
+          past_history: 'New York No. 1 Lake Park',
+          birthday: '2016-10-03'
+        },
+        {
+          name: 'John Brown',
+          sex: 18,
+          past_history: 'New York No. 1 Lake Park',
+          birthday: '2016-10-03'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/view/symptom/sysmptom-tree.vue
+++ src/view/symptom/sysmptom-tree.vue
@@ -3,12 +3,53 @@
 </style>
 <template>
   <div class="content">
-    <div class="btn-con">
-      <Button type="primary" class="symptom-btn">添加一级症状</Button>
-      <Button type="primary" class="symptom-btn">添加子症状</Button>
-      <Button class="symptom-btn">删除</Button>
+    <!--左侧布局-->
+    <div class="tree-con">
+        <div class="btn-con">
+            <Button type="primary" class="symptom-btn">添加一级症状</Button>
+            <Button type="primary" class="symptom-btn">添加子症状</Button>
+            <Button class="symptom-btn">删除</Button>
+        </div>
+        <Tree :data="data2" show-checkbox></Tree>
     </div>
-    <Tree :data="data2" show-checkbox></Tree>
+    <!--右侧布局-->
+    <div class="edit-con">
+        <div class="form-con">
+            <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+                <FormItem label="症状名称" prop="name">
+                    <Input  placeholder="请填写症状名称"></Input>
+                </FormItem>
+                <FormItem label="上级症状" prop="name">
+                    <Input  placeholder="请填写上级症状"></Input>
+                </FormItem>
+                <FormItem label="症状编码" prop="name">
+                    <Input  placeholder="" disabled></Input>
+                </FormItem>
+                <FormItem label="症状类型" prop="gender">
+                    <RadioGroup>
+                        <Radio label="0">症状</Radio>
+                        <Radio label="1">诊断结果</Radio>
+                    </RadioGroup>
+                </FormItem>
+                <FormItem label="紧急程度" prop="gender">
+                    <RadioGroup>
+                        <Radio label="0">一般</Radio>
+                        <Radio label="1">紧急</Radio>
+                        <Radio label="2">非常紧急</Radio>
+                    </RadioGroup>
+                </FormItem>
+                <FormItem label="常见类型" prop="gender">
+                    <RadioGroup>
+                        <Radio label="0">是</Radio>
+                        <Radio label="1">否</Radio>
+                    </RadioGroup>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" @click="handleSubmit('formValidate')">修改并保存</Button>
+                </FormItem>
+            </Form>
+        </div>
+    </div>
   </div>
 </template>
 <script>
